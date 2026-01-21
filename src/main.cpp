c504130e77088eaf60656016b91b529e85826b78
Ethan Rudy <ethan.c.rudy@gmail.com>
2026-01-20 22:10:31 -0700

    Restart

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,6 @@
-
 #include <iostream>
+#include "problems.hpp"
 
-#include "../include/problems.hpp"
-
-/*
-	Solutions are build as sub classes
-	To run one, replace the code below
-	with the following template
-
-	PXXX sol;
-	sol.run();
-
-	Replace the XXX with the number
-	of the solution you'd like to run
-	so 23 would be 023, 4 would be 004, etc
-*/
 int main() {
-	P043 sol;
-	sol.run();
-	return 0;
+	std::cout << solve_p001() << std::endl;
 }
---
+++ src/problems.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <iostream>
+
+int solve_p001();
