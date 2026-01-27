346beec385485ec3353ea2b4b1bba3a193fbc9a5
Ethan Rudy <ethan.c.rudy@gmail.com>
2026-01-26 18:26:45 -0700

    Update p028.cpp

--- src/problems/p028.cpp
+++ src/problems/p028.cpp
@@ -9,11 +9,9 @@ int solve_p028() {
 	int n = 1;
 	while (diff < 1002) {
 		for (int i = 0; i < 4; ++i) {
-			// std::cout << n << "\n";
 			n += diff;
 			sum += n;
 		}
-		// std::cout << n << "\n";
 
 		diff += 2;
 	}
